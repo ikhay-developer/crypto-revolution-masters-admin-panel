804a1f8ca04cbc1179274394c4fed63201ed3bc4
daniel-rif <danielonyenwee@gmail.com>
2022-02-08 03:53:05 +0100

    initilaize project -> crypto-revolution-master-admin-panel

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+</script>
+
+<main>
+</main>
+
+<style lang="less" scoped>
+	main {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		/* Extra small devices (phones, 600px and down) */
+        @media only screen and (max-width: 600px) {
+            & {
+				flex-direction: row;
+				flex-wrap: wrap;
+				justify-content: flex-start;
+				align-items: center;
+				align-content: space-between;
+            }
+        }
+
+        /* Small devices (portrait tablets and large phones, 600px and up) */
+        @media only screen and (min-width: 600px) {
+            & {
+				flex-direction: column;
+				flex-wrap: wrap;
+				justify-content: flex-start;
+				align-items: center;
+				align-content: space-between;
+            }
+        }
+
+        /* Medium devices (landscape tablets, 768px and up) */
+        @media only screen and (min-width: 768px) {
+            & {
+				flex-direction: column;
+				flex-wrap: wrap;
+				justify-content: flex-start;
+				align-items: center;
+				align-content: space-between;
+            }
+        }
+
+        /* Large devices (laptops/desktops, 992px and up) */
+        @media only screen and (min-width: 992px) {
+            & {
+				flex-direction: column;
+				flex-wrap: wrap;
+				justify-content: flex-start;
+				align-items: center;
+				align-content: space-between;
+            }
+        }
+
+        /* Extra large devices (large laptops and desktops, 1200px and up) */
+        @media only screen and (min-width: 1200px) {
+            & {
+				flex-direction: column;
+				flex-wrap: wrap;
+				justify-content: flex-start;
+				align-items: center;
+				align-content: space-between;
+            }
+        }
+	}
+</style>
