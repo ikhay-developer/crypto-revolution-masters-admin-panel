7386b61e101715cb167941f58abd654612f4b68a
daniel-rif <danielonyenwee@gmail.com>
2022-02-17 00:10:08 +0100

    🛠 create component -> noConnectionPage

---
+++ src/utils/noConnectionPage.svelte
@@ -0,0 +1,28 @@
+<div>
+    <img src="./img/globe.svg" alt="no connection">
+    <span>No connection</span>
+</div>
+
+<style lang="less" scoped>
+    div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        img {
+            height: 125px;
+            width: 125px;
+        }
+        span {
+            color: #757575;
+            padding: 5px;
+            margin-top: 7px;
+            font-weight: bolder;
+            font-size: 35px;
+        }
+    }
+</style>
